929887fb682c7745d4bfe3675f131fc9f9770514
Alex Rabochy <alex.rabochy@gmail.com>
2015-01-06 23:09:03 +0300

    geometry+GUI added

---
+++ geom+gui/geom+gui/Form.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace geom_gui
+{
+    partial class Form
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.x11box = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.y11box = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.x12box = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.y12box = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.x21box = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.y21box = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.x22box = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.y22box = new System.Windows.Forms.TextBox();
+            this.res = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1029, 642);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 109);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Let`s check it!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 644);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "x11";
+            // 
+            // x11box
+            // 
+            this.x11box.Location = new System.Drawing.Point(57, 639);
+            this.x11box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.x11box.Name = "x11box";
+            this.x11box.Size = new System.Drawing.Size(115, 31);
+            this.x11box.TabIndex = 2;
+            this.x11box.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 719);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "y11";
+            // 
+            // y11box
+            // 
+            this.y11box.Location = new System.Drawing.Point(57, 711);
+            this.y11box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.y11box.Name = "y11box";
+            this.y11box.Size = new System.Drawing.Size(115, 31);
+            this.y11box.TabIndex = 2;
+            this.y11box.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 644);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "x12";
+            // 
+            // x12box
+            // 
+            this.x12box.Location = new System.Drawing.Point(274, 639);
+            this.x12box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.x12box.Name = "x12box";
+            this.x12box.Size = new System.Drawing.Size(115, 31);
+            this.x12box.TabIndex = 2;
+            this.x12box.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(228, 719);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(47, 25);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "y12";
+            // 
+            // y12box
+            // 
+            this.y12box.Location = new System.Drawing.Point(276, 711);
+            this.y12box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.y12box.Name = "y12box";
+            this.y12box.Size = new System.Drawing.Size(115, 31);
+            this.y12box.TabIndex = 2;
+            this.y12box.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(448, 644);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(47, 25);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "x21";
+            // 
+            // x21box
+            // 
+            this.x21box.Location = new System.Drawing.Point(495, 636);
+            this.x21box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.x21box.Name = "x21box";
+            this.x21box.Size = new System.Drawing.Size(115, 31);
+            this.x21box.TabIndex = 2;
+            this.x21box.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(448, 719);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(47, 25);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "y21";
+            // 
+            // y21box
+            // 
+            this.y21box.Location = new System.Drawing.Point(495, 714);
+            this.y21box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.y21box.Name = "y21box";
+            this.y21box.Size = new System.Drawing.Size(115, 31);
+            this.y21box.TabIndex = 2;
+            this.y21box.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(668, 644);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(47, 25);
+            this.label7.TabIndex = 1;
+            this.label7.Text = "x22";
+            // 
+            // x22box
+            // 
+            this.x22box.Location = new System.Drawing.Point(714, 639);
+            this.x22box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.x22box.Name = "x22box";
+            this.x22box.Size = new System.Drawing.Size(115, 31);
+            this.x22box.TabIndex = 2;
+            this.x22box.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(668, 719);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(47, 25);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "y22";
+            // 
+            // y22box
+            // 
+            this.y22box.Location = new System.Drawing.Point(716, 711);
+            this.y22box.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.y22box.Name = "y22box";
+            this.y22box.Size = new System.Drawing.Size(115, 31);
+            this.y22box.TabIndex = 2;
+            this.y22box.Text = "0";
+            // 
+            // res
+            // 
+            this.res.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.res.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.res.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.res.Location = new System.Drawing.Point(855, 644);
+            this.res.Name = "res";
+            this.res.Size = new System.Drawing.Size(152, 109);
+            this.res.TabIndex = 3;
+            this.res.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1209, 775);
+            this.Controls.Add(this.res);
+            this.Controls.Add(this.y22box);
+            this.Controls.Add(this.y21box);
+            this.Controls.Add(this.y12box);
+            this.Controls.Add(this.y11box);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.x22box);
+            this.Controls.Add(this.x21box);
+            this.Controls.Add(this.x12box);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.x11box);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.LocationChanged += new System.EventHandler(this.Action);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox x11box;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox y11box;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox x12box;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox y12box;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox x21box;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox y21box;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox x22box;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox y22box;
+        private System.Windows.Forms.Label res;
+    }
+}
+
